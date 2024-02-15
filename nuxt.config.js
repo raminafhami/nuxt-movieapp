728a9e9d93177888ebc40909de3611d25cc5cd0a
Ramin <33121210+raminafhami@users.noreply.github.com>
2024-02-15 20:38:40 +0330

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import   components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
